af4d58c6763b2c1f09769613c70dddb65e72c82b
Daniel Luna <daniellunsc@gmail.com>
2018-06-23 03:22:31 -0300

    created some logs

--- services/passport.js
+++ services/passport.js
@@ -6,10 +6,12 @@ const keys = require('../config/keys');
 const User = mongoose.model('users');
 
 passport.serializeUser((user, done) => {
+  console.log(user)
   done(null, user.id);
 });
 
 passport.deserializeUser((id, done) => {
+  console.log(id)
   User.findById(id)
     .then(user => {
       done(null, user);
@@ -26,8 +28,10 @@ passport.use(new GoogleStrategy({
     .then((existingUser) => {
       if (existingUser) {
         // already have a record with given profile id
+        console.log('already exists a user');
         done(null, existingUser);
       } else {
+        console.log('going to create new user');
         new User({ googleId: profile.id })
         .save()
         .then(user => done(null, user))
